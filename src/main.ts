f4ff5ec7459434d6ac0f79e849617488feb0281b
Aleksey Tarasenko <alekstar79@yandex.ru>
2026-03-19 13:59:51 +0400

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createLightbox } from './factory'
 import './styles/main.scss'
 
+// Bootstrap lightbox
 window.addEventListener('DOMContentLoaded', () => {
   const source = Array.from({ length: 28 }, (_, i) => ({
     src: `img-${`${i + 1}`.padStart(2, '0')}.jpg`,
